cec14825094c8c7f538f048528e4c43eb02a81ee
Jordan Hayes <nosafoundation@gmail.com>
2019-11-21 05:25:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corset" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
@@ -15,7 +15,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -25,7 +25,8 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
@@ -33,5 +34,9 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest["whale bone corsets"]
+shipping_manifest["porcelain vases"]
+shipping_manifest["oil paintings"]
+shipping_manifest["muskets"]
+shipping_manifest["gun powder"]
 end
