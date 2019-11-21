ff134829bcf655aa362e3083aeafaef2411f8173
Jordan Hayes <nosafoundation@gmail.com>
2019-11-21 05:33:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,9 +13,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest["oil paintings"]
+                 shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -24,15 +22,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"] = 2
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] = 4
-
-
-  # return the shipping_manifest hash below
-  puts shipping_manifest
+                 shipping_manifest["muskets"] = 2
+                 shipping_manifest["gun powder"] = 4
+                 puts shipping_manifest
 end
